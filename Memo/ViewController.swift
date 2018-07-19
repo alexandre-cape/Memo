d91fbcd8105fe871bc5852a7ace3053afc19add2
Alexandre EL ASSAD <alexandre@cape.com>
2018-07-19 13:01:08 -0700

    Initial Commit

---
+++ Memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memo
+//
+//  Created by Alexandre EL ASSAD on 7/19/18.
+//  Copyright © 2018 Alexandre EL ASSAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
